4d8be17e07b6dc95cea4c6d46884d11e9764fe05
Nicolás Girardi <ngirardi@fi.uba.ar>
2026-07-03 18:43:50 -0300

    pasa el nuevo escenario

--- src/fastAcceptanceTest/java/ar/com/grupoesfera/repartir/steps/grupos/NoSePuedeAgregarSaldoNegativo.java
+++ src/fastAcceptanceTest/java/ar/com/grupoesfera/repartir/steps/grupos/NoSePuedeAgregarSaldoNegativo.java
@@ -1,5 +1,6 @@
 package ar.com.grupoesfera.repartir.steps.grupos;
 
+import ar.com.grupoesfera.repartir.exceptions.MontoNegativoException;
 import ar.com.grupoesfera.repartir.model.Gasto;
 import ar.com.grupoesfera.repartir.model.Grupo;
 import ar.com.grupoesfera.repartir.services.MontosService;
@@ -17,6 +18,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class NoSePuedeAgregarSaldoNegativo extends FastCucumberSteps {
 
     private Grupo grupo;
+    private final MontosService montos = new MontosService();
+    private RuntimeException excepcion;
 
     @Dado("que existe un grupo con saldo en {int}")
     public void queHayUnGrupoConSaldoTotalIgualA(int monto) {
@@ -26,15 +29,23 @@ public class NoSePuedeAgregarSaldoNegativo extends FastCucumberSteps {
 
     @Cuando("uno de sus usuarios intenta sumar un monto de {int}")
     public void elUsuarioIntentaAgregarUnSaldoDe(int monto) {
-
+        try {
+            Gasto gasto = new Gasto();
+            gasto.setMonto(BigDecimal.valueOf(monto));
+            montos.acumularAlTotal(grupo, gasto);
+        } catch (MontoNegativoException e) {
+            excepcion = e;
+        }
     }
 
     @Entonces("no se suma el saldo al grupo")
     public void noSeAgregaElSaldo() {
-
+        assertThat(excepcion)
+                .isNotNull()
+                .isInstanceOf(MontoNegativoException.class);
     }
     @Y("el total debe permanecer en {int}")
-    public void elTotalPermaneceEnCero() {
-
+    public void elTotalPermaneceEnCero(int saldo) {
+        assertThat(grupo.getTotal()).isEqualTo(BigDecimal.valueOf(saldo));
     }
 }
--- src/fastAcceptanceTest/resources/features/grupos/nuevos_gastos.feature
+++ src/fastAcceptanceTest/resources/features/grupos/nuevos_gastos.feature
@@ -3,7 +3,7 @@
 Característica: sumar gastos nuevos a un grupo existente
 
   Regla: La suma total de gastos de un grupo no puede ser menor a cero
-    @wip
+
     Escenario: No puedo sumar un gasto menor a cero a un saldo cero
       Dado que existe un grupo con saldo en 0
       Cuando uno de sus usuarios intenta sumar un monto de -5
